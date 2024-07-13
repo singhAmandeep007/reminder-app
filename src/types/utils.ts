3e9feccd724fb4daa11bf390a24ee6badb9241b3
singhAmandeep007 <amandeeps0803@gmail.com>
2024-07-13 15:38:13 +0530

    refactor: ♻️ Add TOnly and TEither utility types and consistent test file names

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,1 +1,9 @@
 export type TNonEmptyArray<T> = T[] & { 0: T };
+
+export type TOnly<T, U> = {
+  [P in keyof T]: T[P];
+} & {
+  [P in keyof U]?: never;
+};
+
+export type TEither<T, U> = TOnly<T, U> | TOnly<U, T>;
