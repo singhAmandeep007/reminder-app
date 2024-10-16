fca14d0a756dedd7c4eeac83821f5ab337dd8dc9
singhAmandeep007 <amandeeps0803@gmail.com>
2024-10-16 22:49:15 +0530

    refactor: ♻️ Update lint-staged configuration

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-  "**/*.{js,jsx,ts,tsx,css,scss,html,md,json}": () => ["npm run tsc:check", "npm run lint", "npm run test"],
+  "src/**/*.{js,jsx,ts,tsx,css,scss,html,md,json}": () => ["npm run tsc:check", "npm run lint", "npm run test"],
 };
